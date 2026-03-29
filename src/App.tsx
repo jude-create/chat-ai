70252e0c9514a12d60f3d331948505fcac61a1ca
jude-create <ijude236@gmail.com>
2026-03-29 19:38:10 +0100

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ChatMessages from "./ChatMessages";
 import useChatBot from "./hooks/ChatBot";
 import StatusBadge from "./StatusBadge";
@@ -5,24 +6,28 @@ import Suggest from "./Suggest";
 import Typing from "./Typing"
 
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
-
+import brainAnimation from '/images/Brain Network.lottie?url';
 
 function App() {
    const { messages, sendMessage, loading } = useChatBot();
+   const [loaded, setLoaded] = useState(true);
 
   return (
     <>
      <div className="bg-gray-900 min-h-screen w-full ">
       <div className="flex justify-between pt-5 px-3">
       <div className="flex items-center ">
-       <div className="w-18">
-         <DotLottieReact
-            src="/images/Brain Network.lottie"
-            loop
-            autoplay
-             className='w-24'
-           />
-       </div>
+      
+
+   <div className={`w-24 transition-opacity ${loaded ? 'opacity-100' : 'opacity-0'}`}>
+    <DotLottieReact
+      src={brainAnimation}
+    loop
+    autoplay
+      onLoad={() => setLoaded(false)}
+    className='w-24'
+  />
+</div>
        <div>
           <h1 className="text-xl text-cyan-50">AI Chatbot</h1>
        </div>
--- src/Suggest.tsx
+++ src/Suggest.tsx
@@ -1,14 +1,18 @@
 
 import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import brainAnimation from '/images/Brain Network.lottie?url';
+import { useState } from 'react';
 function Suggest() {
+  const [loaded, setLoaded] = useState(true);
   return (
     
         <div className="flex flex-col items-center justify-center pt-16 gap-8 animate-[fadeUp_0.5s_ease_both]">
-      <div >
+      <div className={` transition-opacity ${loaded ? 'opacity-100' : 'opacity-0'}`} >
       <DotLottieReact
-            src="/images/Brain Network.lottie"
+            src={brainAnimation}
             loop
             autoplay
+            onLoad={() => setLoaded(false)}
              className='w-56'
            />
         <h2 className="text-3xl font-bold text-zinc-100 tracking-tight mb-2">What's on your mind?</h2>
